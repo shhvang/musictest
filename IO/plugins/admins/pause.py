95b61188bf436f58184f729c31cf7282596d6e95
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-29 21:02:47 +0530

    test5

--- IO/plugins/admins/pause.py
+++ IO/plugins/admins/pause.py
@@ -8,7 +8,10 @@ from IO.utils.decorators import AdminRightsCheck
 from IO.utils.inline import close_markup
 from config import BANNED_USERS
 
-
+def pause_message(client: Client, message: Message):
+    reply_message = message.reply_to_message
+    if message.text.lower() == "hey io pause":
+        return True
 
 @app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
@@ -22,17 +25,16 @@ async def handle_message(cli, message: Message, _):
     _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
 
-@app.on_message(filters.text([
-    "hey io pause",
-    "Hey io pause"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
-async def pause_hey(cli, message: Message, _):
-    if message.text.lower().startswith("hey io pause"):
-        chat_id = messgae.chat.id
+async def pause_io(cli, message: Message):
+    reply_message = await pause_message(client, message)
+    if reply_message:
         if not await is_music_playing(chat_id):
             return await message.reply_text(_["admin_1"])
         await music_off(chat_id)
         await IOMusic.pause_stream(chat_id)
         await message.reply_text(
-            _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
-        )
+        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+    )
+
+app.add_handler(MessageHandler(pause_io, filters.text))
