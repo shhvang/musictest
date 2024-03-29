96f9f2f0eee674de2fc6b006337c1d59df1adb95
Shivang <132191347+shhvang@users.noreply.github.com>
2024-03-29 17:48:30 +0530

    test4

--- IO/plugins/admins/pause.py
+++ IO/plugins/admins/pause.py
@@ -9,7 +9,7 @@ from IO.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.text([r"hey io pause"]) & filters.group & ~BANNED_USERS)
+
 @app.on_message(filters.command(["pause", "cpause"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def handle_message(cli, message: Message, _):
@@ -21,3 +21,18 @@ async def handle_message(cli, message: Message, _):
     await message.reply_text(
     _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
+
+@app.on_message(filters.text([
+    "hey io pause",
+    "Hey io pause"]) & filters.group & ~BANNED_USERS)
+@AdminRightsCheck
+async def pause_hey(cli, message: Message, _):
+    if message.text.lower().startswith("hey io pause"):
+        chat_id = messgae.chat.id
+        if not await is_music_playing(chat_id):
+            return await message.reply_text(_["admin_1"])
+        await music_off(chat_id)
+        await IOMusic.pause_stream(chat_id)
+        await message.reply_text(
+            _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+        )
